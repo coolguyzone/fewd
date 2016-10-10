8c429a6604dfeba5e0d0959e10848fb4de17b457
coolguyzone <alexanderkrawiec@gmail.com>
2016-10-10 07:59:59 -0700

    finished movie hw

--- movie-favorites/js/movie-favoritesjs.js
+++ movie-favorites/js/movie-favoritesjs.js
@@ -6,12 +6,48 @@
 
 
 
-   $.get("http://omdbapi.com/?i=tt0071615", displayResults);
+//    $.get("http://omdbapi.com/?i=tt0071615", displayResults);
+//
+//    function displayResults(results) {
+//   console.log(results.Title);
+//   console.log(results.Year);
+//   console.log(results.Rated);
+//   console.log(results.Runtime);
+//   console.log(results.Plot);
+// }
 
-   function displayResults(results) {
-  console.log(results.Title);
-  console.log(results.Year);
-  console.log(results.Rated);
-  console.log(results.Runtime);
-  console.log(results.Plot);
+
+//Get one working without the loop or event.target . . .
+// function getMovieData(event) {
+//   $.get("http://omdbapi.com/?i=tt0071615", displayResults);
+//
+//   function displayResults(results) {
+//     document.querySelector("#movie-title").textContent = results.Title;
+//     document.querySelector("#movie-year").textContent = results.Year;
+//     document.querySelector("#movie-rating").textContent = results.Rated;
+//     document.querySelector("#movie-runtime").textContent = results.Runtime;
+//     document.querySelector("#movie-plot").textContent = results.Plot;
+//
+//   }
+// }
+// document.querySelector(".holymountain").addEventListener("click", getMovieData);
+
+//DO IT WITH THE LOOP AND EVENT.TARGET
+
+function getMovieData(event) {
+  $.get("http://omdbapi.com/?i=" + event.target.id, displayResults);
+
+  function displayResults(results) {
+    document.querySelector("#movie-title").textContent = results.Title;
+    document.querySelector("#movie-year").textContent = results.Year;
+    document.querySelector("#movie-rating").textContent = results.Rated;
+    document.querySelector("#movie-runtime").textContent = results.Runtime;
+    document.querySelector("#movie-plot").textContent = results.Plot;
+  }
+}
+
+posters = document.querySelectorAll("img");
+
+for (var i = 0; i < posters.length; i++) {
+  posters[i].addEventListener("click", getMovieData);
 }
