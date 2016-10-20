272c8898596ed735d2dbc4a549608b27567e0217
A K <alexanderkrawiec@gmail.com>
2016-10-20 16:14:23 -0700

    added final shuffle

--- js/js.js
+++ js/js.js
@@ -87,6 +87,13 @@ function removeFoodOption(event) {
     finalOption.removeEventListener("click", removeFoodOption);
     var finalHeading = document.querySelector("h1");
     finalHeading.textContent = "IT HAS BEEN DECIDED!";
+    var startOver = document.createElement("div");
+    startOver.setAttribute("class", "shuffle");
+    document.querySelector("main").appendChild(startOver);
+    newSpan = document.createElement("span");
+    newSpan.textContent = "Wanna Try Again?";
+    startOver.appendChild(newSpan);
+    startOver.addEventListener("click", shuffleFoods);
     }
 
   else if (turn === 2) {
@@ -154,23 +161,27 @@ shuffle.addEventListener("click", shuffleFoods);
 
 
 //OPEN ISSUES:
-//After a few shuffles, we start running out of colors and eventually run out of food options . . .
-//-less than perfect fix: add more colors and foods to allow more shuffles before running out of options
-//-less than perfect fix: have the whole process start over with a new array on each shuffle(but you would get repeat foods)
-
-//Would be nice to have some animations upon removeFoodOption rather than just having the DIVs dissappear
-
-//Could make the messaging if/else statement more concise using modulos
 
 //Make sizes work better on desktop and mobile, perhaps using media queries?
+  //*added media queries, still unsure if sizes are optimal
 
-//How can I make the DIVs animate before disappearing off the screen?
 
 //Add a 'start over' button at the end
 
 //Make the game playable for more than 2 players
 
 //Add a copyright
+  //*added a copyright, but not thrilled with the way it looks
+
+//Decide on a color scheme
+
+//Turn this into an app
+
+//Add recipe suggestions?
+
+//Add a restart button
+  //*Added another shuffle link at the end of the game
+
 
 
 //CLOSED ISSUES:
@@ -195,9 +206,14 @@ shuffle.addEventListener("click", shuffleFoods);
 //Finish adding emojis
   //*emojis added
 
+  //After a few shuffles, we start running out of colors and eventually run out of food options . . .
+  //-less than perfect fix: add more colors and foods to allow more shuffles before running out of options
+  //-less than perfect fix: have the whole process start over with a new array on each shuffle(but you would get repeat foods)
+    //*Decided to make a shuffle reload the page, having some repeats actually seems like it should be expected behavior.
+
+  //Would be nice to have some animations upon removeFoodOption rather than just having the DIVs dissappear
+    //Added animations
 
 
-
-  //have colors start from scratch on refresh
-  //instead of yelp api try using a google search
-  //google places/maps api
+  //Could make the messaging if/else statement more concise using modulos
+    //*Added modulos to minimize code
