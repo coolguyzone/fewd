b7d5d3a404211e609bcbe156e27dafe56587cc82
A K <alexanderkrawiec@gmail.com>
2016-11-16 16:34:36 -0800

    buttons
    fixed button background bugs on tip calculator

--- tips2/js/js.js
+++ tips2/js/js.js
@@ -4,12 +4,17 @@ var tip5 = document.querySelector(".tip5");
   var tip15 = document.querySelector(".tip15");
   var tip20 = document.querySelector(".tip20");
   var tip25 = document.querySelector(".tip25");
-  // var tipDiv5 = document.querySelector(".tipdiv5")
-
-
-
-
-  var tipAmounts = document.querySelector(".tip-amount");
+var tipDiv5 = document.querySelectorAll(".tipdiv5");
+var tipDiv10 = document.querySelectorAll(".tipdiv10");
+var tipDiv15 = document.querySelectorAll(".tipdiv15");
+var tipDiv20 = document.querySelectorAll(".tipdiv20");
+var tipDiv25 = document.querySelectorAll(".tipdiv25");
+var splitDiv1 = document.querySelectorAll(".splitdiv1");
+var splitDiv2 = document.querySelectorAll(".splitdiv2");
+var splitDiv3 = document.querySelectorAll(".splitdiv3");
+var splitDiv4 = document.querySelectorAll(".splitdiv4");
+var splitDiv5 = document.querySelectorAll(".splitdiv5");
+var tipAmounts = document.querySelector(".tip-amount");
   var amount = document.querySelector("input");
   var tipdivs = document.querySelectorAll(".tip-buttons");
   var tipsplit = document.querySelector(".tip-split");
@@ -49,26 +54,40 @@ function calculateTips(event){
       function setTipAMT(event) {
 
         tipsplit.classList.remove('hide');
-        event.target.setAttribute('style', 'background-color: crimson;');
 
         if ($(event.target).hasClass('tipdiv5')) {
+          for (var i = 0; i < tipDiv5.length; i++) {
+            tipDiv5[i].setAttribute('style', 'background-color: crimson;');
+          }
           tip = tip5.textContent;
           console.log(tip);
 
         }
         else if ($(event.target).hasClass('tipdiv10')) {
+          for (var i = 0; i < tipDiv10.length; i++) {
+            tipDiv10[i].setAttribute('style', 'background-color: crimson;');
+          }
           tip = tip10.textContent;
           console.log(tip);
         }
         else if ($(event.target).hasClass('tipdiv15')) {
+          for (var i = 0; i < tipDiv15.length; i++) {
+            tipDiv15[i].setAttribute('style', 'background-color: crimson;');
+          }
           tip = tip15.textContent;
           console.log(tip);
         }
         else if ($(event.target).hasClass('tipdiv20')) {
+          for (var i = 0; i < tipDiv20.length; i++) {
+            tipDiv20[i].setAttribute('style', 'background-color: crimson;');
+          }
           tip = tip20.textContent;
           console.log(tip);
         }
         else if ($(event.target).hasClass('tipdiv25')) {
+          for (var i = 0; i < tipDiv25.length; i++) {
+            tipDiv25[i].setAttribute('style', 'background-color: crimson;');
+          }
           tip = tip25.textContent;
           console.log(tip);
         }
@@ -102,26 +121,41 @@ function calculateTips(event){
     event.target.setAttribute('style', 'background-color: crimson;');
 
     if ($(event.target).hasClass('splitdiv1')) {
+      for (var i = 0; i < splitDiv1.length; i++) {
+        splitDiv1[i].setAttribute('style', 'background-color: crimson;');
+      }
       $(splitH2).addClass('hide');
 
       console.log(totalBill);
     }
     else if ($(event.target).hasClass('splitdiv2')) {
+      for (var i = 0; i < splitDiv2.length; i++) {
+        splitDiv2[i].setAttribute('style', 'background-color: crimson;');
+      }
       totalPer.textContent = ((amount + tip) / 2).toFixed(2);
       console.log(tip);
     }
 
     else if ($(event.target).hasClass('splitdiv3')) {
+      for (var i = 0; i < splitDiv3.length; i++) {
+        splitDiv3[i].setAttribute('style', 'background-color: crimson;');
+      }
       totalPer.textContent = ((amount + tip) / 3).toFixed(2);
       console.log(tip);
     }
 
     else if ($(event.target).hasClass('splitdiv4')) {
+      for (var i = 0; i < splitDiv4.length; i++) {
+        splitDiv4[i].setAttribute('style', 'background-color: crimson;');
+      }
       totalPer.textContent = ((amount + tip) / 4).toFixed(2);
       console.log(tip);
     }
 
     else if ($(event.target).hasClass('splitdiv5')) {
+      for (var i = 0; i < splitDiv5.length; i++) {
+        splitDiv5[i].setAttribute('style', 'background-color: crimson;');
+      }
       totalPer.textContent = ((amount + tip) / 5).toFixed(2);
       console.log(tip);
     }
@@ -133,3 +167,7 @@ function calculateTips(event){
 
 
 document.querySelector(".amt-btn").addEventListener("click", calculateTips);
+
+// To Fix:
+// Make buttons turn red when clicked anywhere, and turn blue again when another button is selected. Make sure space between buttons does NOT turn red.
+// *fixed with a shit ton more variables and fixed loops. how can my code be simplified?
